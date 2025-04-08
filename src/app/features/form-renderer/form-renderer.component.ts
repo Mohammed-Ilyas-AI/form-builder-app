4505ad9dc96b4e293c9e3ad81a33d66483b351be
Mohammed Ilyas <ilyas.fa007@gmail.com>
2025-04-08 18:15:36 +0530

    Add initial implementation of form builder components and services with Tailwind CSS integration

---
+++ src/app/features/form-renderer/form-renderer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-renderer',
+  imports: [],
+  templateUrl: './form-renderer.component.html',
+  styleUrl: './form-renderer.component.css'
+})
+export class FormRendererComponent {
+
+}
