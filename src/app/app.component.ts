2256bed9cdd84fc404d12d4afe0bcf908f0d9ab0
Mohammed Ilyas <ilyas.fa007@gmail.com>
2025-04-08 19:55:30 +0530

    three-pane layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LeftPaneComponent } from './components/left-pane/left-pane.component';
+import { MiddlePaneComponent } from './components/middle-pane/middle-pane.component';
+import { RightPaneComponent } from './components/right-pane/right-pane.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LeftPaneComponent, MiddlePaneComponent, RightPaneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
