d6a165b674c717208512e07bae000fec0a91b395
Mohammed Ilyas <ilyas.fa007@gmail.com>
2025-04-10 16:22:12 +0530

    Implement middle pane functionality with form handling and curd capabilities

--- src/app/components/middle-pane/middle-pane.component.ts
+++ src/app/components/middle-pane/middle-pane.component.ts
@@ -1,11 +1,93 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FieldGroup } from '../../models/field-group';
+import { LocalStorageService } from '../../services/local-storage/local-storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-middle-pane',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './middle-pane.component.html',
-  styleUrl: './middle-pane.component.css'
+  styleUrl: './middle-pane.component.css',
 })
 export class MiddlePaneComponent {
+  form!: FormGroup;
+  isEditMode: boolean = false;
+  selectedGroup: FieldGroup | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private storageService: LocalStorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+
+    this.storageService.selectedFieldGroup$.subscribe((group) => {
+      this.selectedGroup = group;
+      if (group) {
+        this.setFieldGroupData(group);
+      } else {
+        this.fieldGroups.clear();
+      }
+    });
+  }
+
+  initializeForm() {
+    this.form = this.fb.group({
+      fieldGroups: this.fb.array([]),
+    });
+  }
+
+  get fieldGroups(): FormArray {
+    return this.form.get('fieldGroups') as FormArray;
+  }
+
+  setFieldGroupData(groupData: FieldGroup) {
+    this.fieldGroups.clear();
+    const fg = this.fb.group({
+      title: [groupData.name || ''],
+      description: [groupData.description || ''],
+    });
+    this.fieldGroups.push(fg);
+  }
+
+  enterEditMode() {
+    this.isEditMode = true;
+  }
+
+  saveChanges() {
+    if (!this.selectedGroup) return;
+
+    const headerData = this.fieldGroups.at(0).value;
+    const updatedGroup: FieldGroup = {
+      ...this.selectedGroup,
+      name: headerData.title,
+      description: headerData.description,
+    };
+    this.storageService.updateSelectedFieldGroup(updatedGroup);
+    this.isEditMode = false;
+  }
+
+  cancelEdit() {
+    if (this.selectedGroup) {
+      this.setFieldGroupData(this.selectedGroup);
+    }
+    this.isEditMode = false;
+  }
+
+  copyGroup() {
+    this.storageService.copySelectedFieldGroup();
+  }
+
+  deleteGroup() {
+    const confirmDelete = confirm(
+      'Are you sure you want to delete this group?'
+    );
+    if (confirmDelete) {
+      this.storageService.deleteSelectedFieldGroup();
+      this.selectedGroup = null;
+      this.form.reset();
+    }
+  }
 }
