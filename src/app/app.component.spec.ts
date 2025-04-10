ec174c350b42355501ebeb452fefc77fc9c4a2c6
Mohammed Ilyas <ilyas.fa007@gmail.com>
2025-04-10 13:26:48 +0530

    Refactor tests
    for AppComponent and LeftPaneComponent to improve structure and coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, form-builder-app');
+    expect(compiled.querySelector('h1')).toBeNull();
   });
 });
